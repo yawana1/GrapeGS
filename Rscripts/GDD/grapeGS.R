a84dae41479c1fd64d83bfe4bc5a10e9eea88b69
Yaw <yawana1@gmail.com>
2021-08-23 19:16:35 -0400

    cross-validation

--- Rscripts/GDD/grapeGS.R
+++ Rscripts/GDD/grapeGS.R
@@ -1,10 +1,11 @@
-phenofile = '/media/yn259/data/research/HC/phenos/GDD.csv'
-genofile = '/media/yn259/data/research/HC/rh/none/matrix.incidence.txt'
-outdir = '/media/yn259/data/research/HC'
+phenofile = '/Users/yn259/Box/research/HC/phenos/GDD.csv'
+genofile = '/Users/yn259/Box/research/HC/rh/none/matrix.incidence.txt'
+outdir = '/Users/yn259/Box/research/HC'
 traits = c('Diff', 'FB', 'LB')
 maf = 0.05
 marker.callrate = 0.2
 ind.callrate = 0.2
+validationFactor = 'Genotype'
 
 library(ASRgenomics)
 library(asreml)
@@ -78,23 +79,26 @@ for(tr in traits){
   Ginv.sparse <- G.inverse(G = pheno.G, sparseform = TRUE)$Ginv
   
   # 7. Cross validation
-  nLines = dim(pheno.P)[1]
-  foldSize=floor(nLines/(10))
-  currentFoldPos=1
   pheno.P$Genotype <- as.factor(pheno.P$Genotype)
   pheno.P$Year <- as.factor(pheno.P$Year)
-  res <- data.frame(pheno.P$trait)
-  res$prediction <- c(0)
+  pheno.idv <- unique(pheno.P[, validationFactor])
+  nLines = length(pheno.idv)
+  foldSize=floor(nLines/(10))
+  currentFoldPos=1
+  res <- data.frame(Genotype = pheno.idv, prediction = numeric(nLines))
   for(i in c(1:10)){
     i = 1
     Y <- pheno.P
     newpos <- min(currentFoldPos+foldSize, nLines+1)
-    Y$trait[currentFoldPos:(newpos-1)] <- NA
-    GBLUP <- asreml(fixed = trait ~ Year, random = ~vm(Genotype, Ginv.sparse),
+    idv <- pheno.idv[currentFoldPos:(newpos-1)]
+    Y$trait[Y$Genotype %in% idv] <- NA
+    # Y$trait[currentFoldPos:(newpos-1)] <- NA
+    GBLUP <- asreml(fixed = trait ~ Year+Flower, random = ~vm(Genotype, Ginv.sparse),
                     residual = ~idv(units), na.action = na.method(y = "include"),
                     data = Y)
     if(GBLUP$converge==TRUE){
       pred = predict(GBLUP, classify = "Genotype", trace=T)$pvals
+      vpredict(GBLUP, h2 ~ V1/(V1 + V2))
       res$prediction[currentFoldPos:(newpos-1)] <- pred$predicted.value[currentFoldPos:(newpos-1)]
       png(file = paste('prediction', i, 'png', sep='.'), width = 1024, height = 1024)
       plot(GBLUP)
